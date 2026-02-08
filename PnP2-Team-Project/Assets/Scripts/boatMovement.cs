2f6dcba328b8686f1fbaa68fe750b7373941cbb9
jozyp <japardovera@student.fullsail.edu>
2026-02-08 16:36:10 -0500

    boatMovement
    I was able to get the boat to move forward and backwards! I put a draw a ray to see where its pointing!

--- PnP2-Team-Project/Assets/Scripts/boatMovement.cs
+++ PnP2-Team-Project/Assets/Scripts/boatMovement.cs
@@ -1,54 +1,134 @@
 using UnityEngine;
 using System.Collections;
+using System;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class boatMovement : MonoBehaviour
 {
+    // component for body
+
     [SerializeField] Rigidbody rb;
-    [SerializeField] LayerMask irgnoreLayer;
 
     // Boat stats
+
     [SerializeField] int HP;
 
     [SerializeField] float moveSpeed;
-    [SerializeField] float speed;
+
+    [SerializeField] float maxSpeed;
+
     [SerializeField] float turnSpeed;
+
     [SerializeField] float drag;
 
+    [SerializeField] Camera boatCamera;
 
-    int HPOrig;
+    [SerializeField] Transform normalCameraPosition;
+    [SerializeField] Transform topDownCameraPosition;
+    [SerializeField] float cameraTransitionSpeed;
+
+
+    private int HPOrig;
    
-    float moveInput;
-    float turnInput;
+    private float moveInput;
+    private float turnInput;
+    private bool isFishing = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         HPOrig = HP;
-        if (rb == null) rb = GetComponent<Rigidbody>();
+
+        // Auto-assign Rigidbody if not set
+
+        if (rb == null) 
+            rb = GetComponent<Rigidbody>();
+
+        // Configure RigidBody
 
         rb.linearDamping = drag;
         rb.angularDamping = drag;
+        rb.useGravity = false;      // No free falling through the water
 
-        
+        // locks the rotation of the X and the Y  
+
+        rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        // Uses the boatsCamera if none, assigned assign
+
+        if (boatCamera == null)
+            boatCamera = Camera.main;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Get the inputs to move Just forward, and turning side to side 
+
         moveInput = Input.GetAxis("Vertical");
         turnInput = Input.GetAxis("Horizontal");
-        
 
-        //Debug.Log(moveInput);
+        // Fallback to key inputs
 
+        if (Input.GetKey(KeyCode.W)) moveInput = 1f;
+        if (Input.GetKey(KeyCode.S)) moveInput = -1f;
+        if (Input.GetKey(KeyCode.A)) turnInput = -1f;
+        if (Input.GetKey(KeyCode.D)) turnInput = 1f;
+
+
+        // Check for the fishing input, which i think is spacebar from what ben said
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isFishing)
+        {
+            CheckForFishingSpot();
+        }
+
+        // Debug to see what inputs are going through or not
+
+        Debug.Log($"Move: {moveInput}, Turn: {turnInput}, Vel: {rb.linearVelocity.magnitude}, IsKinematic: {rb.isKinematic}");
+
+
+    }
+
+    void FixedUpdate()
+    {
+        movement();
         
     }
+
 
     void movement()
     {
-        rb.AddForce(transform.forward * moveInput * moveSpeed, ForceMode.Acceleration);
 
-        rb.AddTorque(turnInput * turnSpeed * Vector3.up);
+        Vector3 forceDirection = transform.forward * moveInput * moveSpeed;
+
+        rb.AddForce(forceDirection, ForceMode.Force);
+
+        rb.AddTorque(Vector3.up * turnInput * turnSpeed);
+
+        // Apply forces of torq
+
+        if (Mathf.Abs(moveInput) > 0.1f || rb.linearVelocity.magnitude > 0.5f)
+        {
+            rb.AddTorque(Vector3.up * turnInput * turnSpeed);
+        }
+
+        // Cap the speed max
+
+        if (rb.linearVelocity.magnitude > maxSpeed)
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+        }
+
+        Debug.DrawRay(transform.position, forceDirection.normalized * 5f, Color.green);
+       
     }
+
+    void CheckForFishingSpot()
+    {
+        throw new NotImplementedException();
+    }
+
 }
